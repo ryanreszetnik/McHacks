89aeb37f2df5a9947cec7dc46235b3d6b8828f0a
ryanreszetnik <rres@rogers.com>
2022-01-22 15:09:15 -0500

    auto log in

--- youredoinggreat/src/App.js
+++ youredoinggreat/src/App.js
@@ -1,16 +1,20 @@
 import logo from "./logo.svg";
 import "./App.css";
 import Signup from "./Authentication/Signup";
-import Amplify from "aws-amplify";
+import Amplify, { Auth } from "aws-amplify";
 import { awsConfig } from "./credentials";
 import ConfirmPhone from "./Authentication/ConfirmPhone";
 import Signin from "./Authentication/Signin";
 import { Button } from "@mui/material";
 import { appLoad, callMe, textMe } from "./Endpoints";
+import { useDispatch } from "react-redux";
+import { SET_USER } from "./Constants/reducerEvents";
+import { useEffect } from "react";
 
 Amplify.configure(awsConfig);
 
 function App() {
+  const dispatch = useDispatch();
   const send = async () => {
     console.log(await appLoad());
   };
@@ -22,6 +26,13 @@ function App() {
   const text = async () => {
     console.log(await textMe());
   };
+  const login = async () => {
+    const usersession = await Auth.currentSession();
+    dispatch({ type: SET_USER, payload: usersession });
+  };
+  useEffect(() => {
+    login();
+  }, []);
   return (
     <div className="App">
       <Signup />
