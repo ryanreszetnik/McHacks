b4e4ac90c0b4e1ffd012e57a5f84b36d66d1ef0a
ryanreszetnik <rres@rogers.com>
2022-01-23 04:03:24 -0500

    who knows anymore

--- youredoinggreat/src/Components/Header/Header.jsx
+++ youredoinggreat/src/Components/Header/Header.jsx
@@ -6,7 +6,8 @@ import { SET_USER } from "../../Constants/reducerEvents";
 import Tab from "./Tab";
 import "./header.css";
 import { useLocation } from "react-router-dom";
-import logo from "../../images/output-onlinepngtools.png"
+import logo from "../../images/output-onlinepngtools.png";
+import CustomButton from "../CustomButton";
 
 const pages = [
   { name: "Home", link: "/" },
@@ -21,26 +22,23 @@ export default function Header() {
     dispatch({ type: SET_USER, payload: null });
   };
   return (
-    
     <div
-    className="headerBox"
+      className="headerBox"
       style={{
         width: "100%",
         height: "80px",
         display: "flex",
       }}
     >
-
       <div className="appTitle">you're doing great</div>
       <img className="logo" src={logo}></img>
 
-
-  <div 
-    style={{
-      paddingLeft: "100px",
-      paddingRight: "80px",
-    }}
-    >
+      <div
+        style={{
+          paddingLeft: "100px",
+          paddingRight: "80px",
+        }}
+      >
         {pages.map((p) => (
           <Tab
             name={p.name}
@@ -49,10 +47,10 @@ export default function Header() {
             selected={pathname === p.link}
           />
         ))}
-        
-        </div>
-        <button className="signOutButton" onClick={signOut}>Sign Out</button>
-  </div>
-
+      </div>
+      <div style={{ paddingTop: "30px" }}>
+        <CustomButton onClick={signOut} label="Sign Out" />
+      </div>
+    </div>
   );
 }
